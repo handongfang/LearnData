75b7b7afe13e511242f8d14042ff12adc80a2e24
handongfang <82168448+handongfang@users.noreply.github.com>
2023-04-03 06:43:20 +0000

    Deploying to gh-pages from @ handongfang/LearnData@11db214986f9546ed4949e796653edcc03a88204 🚀

--- assets/pageview-54570e30.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./app-072c1129.js";import"./framework-d651fda7.js";const n=()=>t({serverURL:"https://waline.newzone.top"});export{n as updatePageview};
---
+++ assets/pageview-c94a7dc6.js
@@ -0,0 +1,1 @@
+import{k as t}from"./app-85404fd7.js";import"./framework-d651fda7.js";const n=()=>t({serverURL:"https://app-waline-han.up.railway.app/"});export{n as updatePageview};
